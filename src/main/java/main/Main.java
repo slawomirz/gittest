f6a007bbdfc2b45c92477bb42328d6e1e0f539b1
Sławomir Zając <z4j4c5@gmail.com>
2019-07-26 20:31:27 +0200

    first

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pierwsza wersja.");
+        System.out.println("Druga wersja wersja.");
+        System.out.println("Trzecia wersja wersja.");
+
+        System.out.println("Stash.");
+
+        ////////////////jisjdksjdksjkd
+
+        System.out.println("inne potem" +
+                ".");
+        System.out.println("Stashwwwwwwwwwwwwwwwwwwww.");
+    }
+}
